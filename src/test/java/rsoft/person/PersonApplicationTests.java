242f18a6c4c537b91e49d2083ecf88945a01e7df
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2025-09-12 07:43:10 -0300

    feat:Initial java 21 project

---
+++ src/test/java/rsoft/person/PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package rsoft.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
