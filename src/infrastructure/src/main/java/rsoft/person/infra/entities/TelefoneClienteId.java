ebf918dd52e4b966c595e8fa571cbf07412ef100
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2025-09-15 04:52:23 -0300

    fix:Adjuste packages names

---
+++ src/infrastructure/src/main/java/rsoft/person/infra/entities/TelefoneClienteId.java
@@ -0,0 +1,26 @@
+package rsoft.person.infra.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class TelefoneClienteId implements Serializable {
+
+    private Integer tipoTelefoneId;
+    private Integer clienteId;
+
+    // equals e hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TelefoneClienteId)) return false;
+        TelefoneClienteId that = (TelefoneClienteId) o;
+        return Objects.equals(tipoTelefoneId, that.tipoTelefoneId) &&
+                Objects.equals(clienteId, that.clienteId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tipoTelefoneId, clienteId);
+    }
+
+}
